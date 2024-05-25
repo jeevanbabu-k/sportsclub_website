91456bf9015a7984c9c609c190e21cac26d0fad5
jeevanbabu-k <jeevanbabuk20@gmail.com>
2024-05-25 22:29:37 +0530

    nm

--- sportsapp/urls.py
+++ sportsapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.loginPage, name='login'),
     path('about_us/<str:pk>/', views.about_us,name='about_us'),
 ]
--- sportsapp/views.py
+++ sportsapp/views.py
@@ -3,10 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-about_us=[
+users=[
     {'id':1, 'name': 'jeevan'},
     {'id':12, 'name': 'isaac'},
 ]
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+    return render(request, 'sportsapp/login_register.html')
+
 
 def home(request):
     context =  {'about_us':about_us}
@@ -14,6 +21,5 @@ def home(request):
 
 
 def about_us(request,pk):
-    
     return render(request,'sportsapp/about_us.html')
 
